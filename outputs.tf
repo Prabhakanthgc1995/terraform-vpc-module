02233cfc1a5e447b70bed6f02c78c7cb918b9333
Prabhakanthgc1995 <prabhakanth0811@gmail.com>
2025-03-06 22:52:01 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
